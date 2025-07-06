1c6f0636930a1085cc3c8bc6f2e8734b643afc79
Simon <simon@fa-loeffler.de>
2025-07-06 22:31:45 +0200

    fehler env richtig benannt

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,5 +9,5 @@
 export const config = {
     // TODO: wen env existiert prüfen das am schluss kein slash ist
     // backendUrl: getEnv('NEXT_PUBLIC_BACKEND_URL') || 'http://localhost:8000',
-    backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8000',
+    backendUrl: process.env.BACKEND_URL ?? 'http://localhost:8000',
 };
